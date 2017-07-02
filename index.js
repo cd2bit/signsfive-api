3b620063d002b380c19ef5d78932636eaba1489c
Giordon Stark <kratsg@gmail.com>
2017-07-02 12:20:01 -0500

    add dotenv and bodyparser plugin

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// https://www.npmjs.com/package/dotenv
+require('dotenv').config();
+
 var restify = require('restify');
 var server = restify.createServer();
+
+server.use(restify.plugins.bodyParser());
 
 server.get('/', function(req, res, next){
   res.send({'message': 'welcome to the api'});
