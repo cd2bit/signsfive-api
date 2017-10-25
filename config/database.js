996ad082f973d4d3e08a988b3e1cd7b600ea329a
Giordon Stark <kratsg@gmail.com>
2017-10-24 21:38:28 -0500

    update config to js format

--- config/database.js
+++ config/database.js
@@ -1,24 +1,24 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+module.exports = {
+  development: {
+    username: 'database_dev',
+    password: 'database_dev',
+    database: 'database_dev',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  test: {
+    username: 'database_test',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false
   },
-  "production": {
-    "use_env_variable": "CLEARDB_DATABASE_URL",
-    "dialect": "mysql",
-    "pool": {"min": 0, "max": 10, "idle": 100},
-    "operatorsAliases": false
+  production: {
+    use_env_variable: 'CLEARDB_DATABASE_URL',
+    dialect: 'mysql',
+    pool: {min: 0, max: 10, idle: 100},
+    operatorsAliases: false
   }
 }
