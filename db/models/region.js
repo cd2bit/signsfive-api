2e5f037f5e88265a9f756c9e751a13cba2cef5bb
Giordon Stark <kratsg@gmail.com>
2017-11-21 14:54:07 -0600

    update region

--- db/models/region.js
+++ db/models/region.js
@@ -2,11 +2,12 @@
 module.exports = (sequelize, DataTypes) => {
   var Region = sequelize.define('Region', {
     name: DataTypes.STRING,
-    description: DataTypes.TEXT
+    loc: DataTypes.GEOMETRY('POINT')
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        Region.belongsToMany(models.Author, {as: "Authors", through: "author_region", foreignKey: "region_id", otherKey: "author_id"});
+        Region.belongsToMany(models.Sign, {as: "Signs", through: "sign_region", foreignKey: "region_id", otherKey: "sign_id"});
       }
     }
   });
