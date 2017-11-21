a7e01e9312d2bb88a01920f32ba2e1999a76de93
Giordon Stark <kratsg@gmail.com>
2017-11-21 15:04:15 -0600

    update category

--- db/models/category.js
+++ db/models/category.js
@@ -5,8 +5,8 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       validate: {
         is: /^[a-z]+( [a-z]+)*$/i,
-	notNull: true,
-	len: [3, 50]
+        notNull: true,
+        len: [3, 50]
       }
     },
     description: {
@@ -16,7 +16,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        Category.belongsToMany(models.Source, {as: "Sources", through: "category_source", foreignKey: "category_id", otherKey: "source_id"});
+        Category.belongsToMany(models.Sign, {as: "Signs", through: "category_sign", foreignKey: "category_id", otherKey: "sign_id"});
       }
     }
   });
