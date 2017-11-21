dd351a349659c9ba72746507d4cbe9f56e72579d
Giordon Stark <kratsg@gmail.com>
2017-11-21 15:18:03 -0600

    update gloss

--- db/models/gloss.js
+++ db/models/gloss.js
@@ -1,12 +1,23 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Gloss = sequelize.define('Gloss', {
-    name: DataTypes.STRING,
-    description: DataTypes.TEXT
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        is: /^[a-z]+( [a-z]+)*$/i,
+        notNull: true,
+        len: [3, 50]
+      }
+    },
+    description: {
+      type: DataTypes.TEXT,
+      notNull: true
+    }
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        Gloss.belongsToMany(models.Source, {as: "Sources", through: "gloss_source", foreignKey: "gloss_id", otherKey: "source_id"});
+        Gloss.belongsToMany(Gloss, {as: "AlternativeGlosses", through: "gloss_gloss", foreignKey: "gloss_id", otherKey: "alt_gloss_id"});
       }
     }
   });
