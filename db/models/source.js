ef569904173d8736bd9e54f08b71d44b9b703411
Giordon Stark <kratsg@gmail.com>
2017-11-21 14:59:46 -0600

    update source

--- db/models/source.js
+++ db/models/source.js
@@ -1,12 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Source = sequelize.define('Source', {
-    name: DataTypes.STRING,
-    description: DataTypes.TEXT
+    url: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: true
+      }
+    }
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        Source.belongsToMany(models.Gloss, {as: "Glosses", through: "gloss_source", foreignKey: "source_id", otherKey: "gloss_id"});
+        Source.belongsToMany(models.Category, {as: "Categories", through: "category_source", foreignKey: "source_id", otherKey: "category_id"});
       }
     }
   });
