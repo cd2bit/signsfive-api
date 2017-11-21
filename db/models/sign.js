03684e77ca7666a9cee013133bc233efb7c5ba52
Giordon Stark <kratsg@gmail.com>
2017-11-21 17:08:49 -0600

    fix up typo in category_sign

--- db/models/sign.js
+++ db/models/sign.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   Sign.associate = models => {
-    Sign.belongsToMany(models.Category, {as: "Categories", through: "category_sign", foreignKey: "sign_id", otherKey: "gloss_id"});
+    Sign.belongsToMany(models.Category, {as: "Categories", through: "category_sign", foreignKey: "sign_id", otherKey: "category_id"});
     Sign.belongsToMany(models.Gloss, {as: "Glosses", through: "gloss_sign", foreignKey: "sign_id", otherKey: "gloss_id"});
     Sign.belongsToMany(models.Region, {as: "Regions", through: "region_sign", foreignKey: "sign_id", otherKey: "region_id"});
   };
