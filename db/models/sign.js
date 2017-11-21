144ebbd1f7b9ad4a9dff5e79e265173dc9406e8e
Giordon Stark <kratsg@gmail.com>
2017-11-21 14:12:22 -0600

    sign updated

--- db/models/sign.js
+++ db/models/sign.js
@@ -1,16 +1,18 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Sign = sequelize.define('Sign', {
-    name: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    path: DataTypes.STRING
+    giphy_id: {
+      type: DataTypes.STRING,
+      validate: {
+        isAlphanumeric: true
+      }
+    }
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
-        // - terminologies
-        // - categories
-        // - introspects
+        Sign.belongsToMany(models.Category, {as: "Categories", through: "sign_category", foreignKey: "sign_id", otherKey: "gloss_id"});
+        Sign.belongsToMany(models.Gloss, {as: "Glosses", through: "sign_gloss", foreignKey: "sign_id", otherKey: "gloss_id"});
+        Sign.belongsToMany(models.Region, {as: "Regions", through: "sign_region", foreignKey: "sign_id", otherKey: "region_id"});
       }
     }
   });
