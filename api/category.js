6ae077ae8ce598b4495ca94cb1b6f7962f8d55a0
Giordon Stark <kratsg@gmail.com>
2018-02-05 09:58:50 +0100

    document category

--- api/category.js
+++ api/category.js
@@ -1,4 +1,17 @@
 module.exports.register = (server) => {
+  /**
+   * @api {get} /categories Get a list of categories
+   * @apiVersion 1.0.0
+   * @apiName  GetCategories
+   * @apiGroup Category
+   * @apiPermission none
+   *
+   * @apiSuccess {Object[]} data        List of categories (Array of Objects)
+   * @apiSuccess {Number}   data.id     Category ID
+   * @apiSuccess {String}   data.name   Category Name
+   * @apiSuccess {String}   data.short  Category Abbreviation
+   *
+   */
   server.get('/categories', function(req, res, next){
     res.send({data: [{ id: 1, name: "science",      short: "s"},
                      { id: 2, name: "technology",   short: "t"},
