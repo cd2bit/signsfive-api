d4c0179cf99e9d8c1eb18447db2387cfc14e3f7f
Giordon Stark <kratsg@gmail.com>
2018-02-05 09:56:04 +0100

    add apidoc overall and document main

--- api/main.js
+++ api/main.js
@@ -1,6 +1,19 @@
 var utils = require('../utils');
 
 module.exports.register = (server) => {
+  /**
+   * @api {get} / Get the available API routes
+   * @apiVersion 1.0.0
+   * @apiName  GetRoutes
+   * @apiGroup Main
+   * @apiPermission none
+   *
+   * @apiSuccess {Object[]} routes          List of routes (Array of Objects)
+   * @apiSuccess {String}   routes.path     Path
+   * @apiSuccess {String}   routes.method   HTTP request type
+   * @apiSuccess {String[]} routes.versions List of versions (Array of Strings)
+   *
+   */
   server.get('/', function(req, res, next){
     // hasOwnProperty avoids inherited properties
     var routes = utils.list_routes(server);
@@ -8,11 +21,31 @@ module.exports.register = (server) => {
     next();
   });
 
+  /**
+   * @api {get} /name Get the name of the API
+   * @apiVersion 1.0.0
+   * @apiName  GetName
+   * @apiGroup Main
+   * @apiPermission none
+   *
+   * @apiSuccess {String} name Name of the project, should be `signsfive-api`
+   *
+   */
   server.get('/name', function(req, res, next){
     res.send({name: process.env.npm_package_name});
     next();
   });
 
+  /**
+   * @api {get} /version Get the version of the API
+   * @apiVersion 1.0.0
+   * @apiName  GetVersion
+   * @apiGroup Main
+   * @apiPermission none
+   *
+   * @apiSuccess {String} version Version of the API being used, should be `1.0.0`
+   *
+   */
   server.get('/version', function(req, res, next){
     res.send({version: process.env.npm_package_version});
     next();
